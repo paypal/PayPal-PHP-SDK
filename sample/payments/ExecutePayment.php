da89241b83efae5a23e22f544b49192fdfacee8b
Kale <kale@kryogenic.org>
2013-11-03 11:48:14 -0800

    Capture the result of $payment->execute
    The important info is not included in the Payment object, but in the object that is returned from $payment->execute

--- sample/payments/ExecutePayment.php
+++ sample/payments/ExecutePayment.php
@@ -29,10 +29,10 @@ if(isset($_GET['success']) && $_GET['success'] == 'true') {
 	
 	//Execute the payment
 	// (See bootstrap.php for more on `ApiContext`)
-	$payment->execute($execution, $apiContext);
+	$result = $payment->execute($execution, $apiContext);
 
 	echo "<html><body><pre>";
-	var_dump($payment->toArray());
+	var_dump($result);
 	echo "</pre><a href='../index.html'>Back</a></body></html>";
 	
 } else {
