12421e09ea5295bf8c41a71e3aa5e800d863c043
japatel <japatel@paypal.com>
2015-01-10 17:21:21 -0600

    PayPal Model toJSON to determine encoding based on PHP Version
    - Allows Preetifying JSON output
    - toString prints a nice looking JSON now

--- lib/PayPal/Common/PayPalModel.php
+++ lib/PayPal/Common/PayPalModel.php
@@ -245,6 +245,9 @@ class PayPalModel
         // Because of PHP Version 5.3, we cannot use JSON_UNESCAPED_SLASHES option
         // Instead we would use the str_replace command for now.
         // TODO: Replace this code with return json_encode($this->toArray(), $options | 64); once we support PHP >= 5.4
+        if (version_compare(phpversion(), '5.4.0', '>=') === true) {
+            return json_encode($this->toArray(), $options | 64);
+        }
         return str_replace('\\/', '/', json_encode($this->toArray(), $options));
     }
 
@@ -255,6 +258,6 @@ class PayPalModel
      */
     public function __toString()
     {
-        return json_encode($this->toArray());
+        return $this->toJSON(128);
     }
 }
