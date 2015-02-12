d50d980cb87002a9161dcd5f0db8dac8bf728448
Jay Patel <japatel@paypal.com>
2015-02-12 15:20:45 -0600

    Updates to API Urls
    - api.sandbox.paypal.com was changed to api-s.sandbox.paypal.com
    - api.paypal.com was changed to api-s.paypal.com

--- lib/PayPal/Core/PayPalConstants.php
+++ lib/PayPal/Core/PayPalConstants.php
@@ -19,9 +19,9 @@ class PayPalConstants
      */
     const APPROVAL_URL = 'approval_url';
 
-    const REST_SANDBOX_ENDPOINT = "https://api.sandbox.paypal.com/";
+    const REST_SANDBOX_ENDPOINT = "https://api-s.sandbox.paypal.com/";
     const OPENID_REDIRECT_SANDBOX_URL = "https://www.sandbox.paypal.com/webapps/auth/protocol/openidconnect";
 
-    const REST_LIVE_ENDPOINT = "https://api.paypal.com/";
+    const REST_LIVE_ENDPOINT = "https://api-s.paypal.com/";
     const OPENID_REDIRECT_LIVE_URL = "https://www.paypal.com/webapps/auth/protocol/openidconnect";
 }
