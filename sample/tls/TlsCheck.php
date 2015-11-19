318d5a43c6eb74c7a91ba82e65bce2f92d565e29
Jay Patel <japatel@paypal.com>
2015-11-19 12:36:53 -0600

    Optimizing Imports
    - Removed Unnecessary imports

--- sample/tls/TlsCheck.php
+++ sample/tls/TlsCheck.php
@@ -7,9 +7,6 @@
 
 require __DIR__ . '/../bootstrap.php';
 use PayPal\Api\Amount;
-use PayPal\Api\Details;
-use PayPal\Api\Item;
-use PayPal\Api\ItemList;
 use PayPal\Api\Payer;
 use PayPal\Api\Payment;
 use PayPal\Api\RedirectUrls;
