1d1f1d46ad02be6596f47ab27f4a7b1e485b883b
Xiaolei Wheelock <xwheelock@paypal.com>
2017-05-02 15:19:41 -0500

    Add getToken() function to Payment class by getting the EC token from the  Approval URL

--- lib/PayPal/Api/Payment.php
+++ lib/PayPal/Api/Payment.php
@@ -553,7 +553,7 @@ class Payment extends PayPalResourceModel
 	public function getToken()
 	{
 		$parameter_name = "token";
-		parse_str(parse_url($this->getApprovalLink())['query'], $query);
+		parse_str(parse_url($this->getApprovalLink(), PHP_URL_QUERY), $query);
 		return (!isset($query[$parameter_name]) || is_null($query[$parameter_name])) ? null : $query[$parameter_name];
 	}
 	
