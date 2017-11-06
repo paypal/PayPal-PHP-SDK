26dadbf879971d1b3e547a9b0ee47ecb09e27bf6
Jay <jaypatel512@gmail.com>
2017-11-06 11:49:36 -0600

    Add HUF as non-decimal currency (#974)
    - Fixes #973.

--- lib/PayPal/Converter/FormatConverter.php
+++ lib/PayPal/Converter/FormatConverter.php
@@ -46,10 +46,10 @@ class FormatConverter
     public static function formatToPrice($value, $currency = null)
     {
         $decimals = 2;
-        $currencyDecimals = array('JPY' => 0, 'TWD' => 0);
+        $currencyDecimals = array('JPY' => 0, 'TWD' => 0, 'HUF' => 0);
         if ($currency && array_key_exists($currency, $currencyDecimals)) {
             if (strpos($value, ".") !== false && (floor($value) != $value)) {
-                //throw exception if it has decimal values for JPY and TWD which does not ends with .00
+                //throw exception if it has decimal values for JPY, TWD and HUF which does not ends with .00
                 throw new \InvalidArgumentException("value cannot have decimals for $currency currency");
             }
             $decimals = $currencyDecimals[$currency];
