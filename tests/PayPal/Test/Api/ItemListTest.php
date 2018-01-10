1950b156efc0686224881e9601f3e07efa1651f8
Benjamin Pick <benjaminpick@github.com>
2018-01-10 14:34:06 +0100

    Adding a test that will fail with the current code

--- tests/PayPal/Test/Api/ItemListTest.php
+++ tests/PayPal/Test/Api/ItemListTest.php
@@ -2,6 +2,7 @@
 
 namespace PayPal\Test\Api;
 
+use PayPal\Api\Item;
 use PayPal\Api\ItemList;
 use PHPUnit\Framework\TestCase;
 
@@ -57,5 +58,24 @@ class ItemListTest extends TestCase
         $this->assertEquals($obj->getShippingAddress(), ShippingAddressTest::getObject());
         $this->assertEquals($obj->getShippingMethod(), "TestSample");
         $this->assertEquals($obj->getShippingPhoneNumber(), "TestSample");
+    }
+	
+	/**
+     * @depends testSerializationDeserialization
+     * @param ItemList $obj
+     */
+    public function testAddRemove($obj)
+    {
+		$item2 = new Item(ItemTest::getJSON());
+		$item2->setSku('TestSample2');
+        $item3 = new Item(ItemTest::getJSON());
+		$item3->setSku('TestSample3');
+		$obj->addItem($item2);
+		$obj->addItem($item3);
+		$this->assertCount(3, $obj->getItems());
+		$obj->removeItem($item2);
+		
+		$this->assertCount(2, $obj->getItems());
+		$this->assertContains('"items":[', $obj->toJSON());
     }
 }
