5936011393b9c3a34a57fcdaf6b43e35b61d6e2e
jaypatel512 <japatel@paypal.com>
2018-02-06 15:16:48 -0600

    Tests re-ordering

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,7 +8,3 @@ ini_set('display_startup_errors', '1');
 if (!ini_get('date.timezone')) {
     date_default_timezone_set('GMT');
 }
-
-// Include the composer autoloader
-$loader = require dirname(__DIR__) . '/vendor/autoload.php';
-$loader->add('PayPal\\Test', __DIR__);
