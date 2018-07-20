ff01896f62222dcd19b64644a923fec37ac18bf4
Barry Deeney <mxaddict@codedmaster.com>
2018-07-20 15:19:48 +0800

    Fixed the issue with NULL values getting passed from PAYPAL API to the UrlValidator

--- lib/PayPal/Api/MerchantPreferences.php
+++ lib/PayPal/Api/MerchantPreferences.php
@@ -80,7 +80,9 @@ class MerchantPreferences extends PayPalModel
      */
     public function setCancelUrl($cancel_url)
     {
-        UrlValidator::validate($cancel_url, "CancelUrl");
+        if (!is_null($cancel_url)) {
+            UrlValidator::validate($cancel_url, "CancelUrl");
+        }
         $this->cancel_url = $cancel_url;
         return $this;
     }
@@ -104,7 +106,9 @@ class MerchantPreferences extends PayPalModel
      */
     public function setReturnUrl($return_url)
     {
-        UrlValidator::validate($return_url, "ReturnUrl");
+        if (!is_null($return_url)) {
+            UrlValidator::validate($return_url, "ReturnUrl");
+        }
         $this->return_url = $return_url;
         return $this;
     }
@@ -128,7 +132,9 @@ class MerchantPreferences extends PayPalModel
      */
     public function setNotifyUrl($notify_url)
     {
-        UrlValidator::validate($notify_url, "NotifyUrl");
+        if (!is_null($notify_url)) {
+            UrlValidator::validate($notify_url, "NotifyUrl");
+        }
         $this->notify_url = $notify_url;
         return $this;
     }
