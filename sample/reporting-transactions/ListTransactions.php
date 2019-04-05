36a012738507e57d5ef09fe1b27487bc265ddc8a
Omar Mohamad - El Hassan Lopesino <omar.lopesino@metadrop.net>
2019-04-05 08:36:25 +0200

    Update list transactions sample

--- sample/reporting-transactions/ListTransactions.php
+++ sample/reporting-transactions/ListTransactions.php
@@ -3,9 +3,9 @@
 require __DIR__ . '/../bootstrap.php';
 use PayPal\Api\ReportingTransactions;
 
-// ### Retrieve payment
-// Retrieve the PaymentHistory object by calling the
-// static `get` method on the Payment class,
+// ### List transactions.
+// Retrieve the reporting transaction list from object by calling the
+// static `get` method on the ReportingTransaction class,
 // and pass a Map object that contains
 // query parameters for paginations and filtering.
 // Refer the method doc for valid values for keys
@@ -13,7 +13,7 @@ use PayPal\Api\ReportingTransactions;
 try {
     $params = array('count' => 100, 'start_date' => '2019-03-20T00:00:00-0700', 'end_date' => '2019-04-10T23:59:59-0700', 'fields' => 'all', 'page' => 1);
 
-    $payments = ReportingTransactions::get($params, $apiContext);
+    $response = ReportingTransactions::get($params, $apiContext);
 } catch (Exception $ex) {
     // NOTE: PLEASE DO NOT USE RESULTPRINTER CLASS IN YOUR ORIGINAL CODE. FOR SAMPLE ONLY
     ResultPrinter::printError("List Payments", "Payment", null, $params, $ex);
@@ -21,4 +21,5 @@ try {
 }
 
 // NOTE: PLEASE DO NOT USE RESULTPRINTER CLASS IN YOUR ORIGINAL CODE. FOR SAMPLE ONLY
-ResultPrinter::printResult("List Payments", "Payment", null, $params, $payments);
+ResultPrinter::printResult("List transactions", "Payment", null, $params, $response);
+ResultPrinter::printResult("Transaction details", "Payment", null, $params, $response->getTransactionDetails());
